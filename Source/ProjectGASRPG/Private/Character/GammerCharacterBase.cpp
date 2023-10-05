6144903745d7b46fbeada016c53d30071bc03f19
liuke101 <226256089@qq.com>
2023-10-05 22:01:12 +0100

    创建角色和敌人的动画蓝图

--- Source/ProjectGASRPG/Private/Character/GammerCharacterBase.cpp
+++ Source/ProjectGASRPG/Private/Character/GammerCharacterBase.cpp
@@ -1,8 +1,15 @@
 ﻿#include "ProjectGASRPG/Public/Character/GammerCharacterBase.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
 AGammerCharacterBase::AGammerCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true; //朝向旋转到移动方向
+	
 	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
 	WeaponMesh->SetupAttachment(GetMesh(), TEXT("WeaponHandSocket"));
 	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
