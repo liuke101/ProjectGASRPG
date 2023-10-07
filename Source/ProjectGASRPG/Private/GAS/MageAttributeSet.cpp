6d62b002b66814fed58f9aa3cbff550446c894b6
liuke101 <226256089@qq.com>
2023-10-07 16:55:10 +0100

    GAS025 定义和初始化Attribute
    编号改为集数

--- Source/ProjectGASRPG/Private/GAS/MageAttributeSet.cpp
+++ Source/ProjectGASRPG/Private/GAS/MageAttributeSet.cpp
@@ -1,2 +1,49 @@
 #include "GAS/MageAttributeSet.h"
 
+#include "Net/UnrealNetwork.h"
+
+UMageAttributeSet::UMageAttributeSet()
+{
+
+	InitHealth(100.0f);
+	InitMaxHealth(100.0f);
+	InitMana(100.0f);
+	InitMaxMana(100.0f);
+}
+
+void UMageAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	/*
+	 * 这里列出我们想要复制的属性
+	 * 
+	 * COND_None：此属性没有条件，并将在其发生变化时随时发送（服务器）
+	 * REPNOTIFY_Always：RepNotify函数在客户端值已经与服务端复制的值相同的情况下也会触发(因为有预测)
+	 */   
+	DOREPLIFETIME_CONDITION_NOTIFY(UMageAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMageAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMageAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMageAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UMageAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	/* 该宏用于 RepNotify 函数，以处理将被客户端预测修改的属性。 */
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMageAttributeSet, Health, OldHealth);
+}
+
+void UMageAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMageAttributeSet, MaxHealth, OldMaxHealth); 
+}
+
+void UMageAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMageAttributeSet, Mana, OldMana);
+}
+
+void UMageAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMageAttributeSet, MaxMana, OldMaxMana)
+}
--- Source/ProjectGASRPG/Public/GAS/MageAttributeSet.h
+++ Source/ProjectGASRPG/Public/GAS/MageAttributeSet.h
@@ -1,13 +1,65 @@
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "MageAttributeSet.generated.h"
 
+/*
+ * 该宏复制自AttributeSet.h
+ * 
+ * 自动为每个Attribute生成以下函数：（以Health为例）
+ * static FGameplayAttribute UMyHealthSet::GetHealthAttribute(); 
+ * FORCEINLINE float UMyHealthSet::GetHealth() const; ==> GetCurrentValue
+ * FORCEINLINE void UMyHealthSet::SetHealth(float NewVal); ==>  SetBaseValue
+ * FORCEINLINE void UMyHealthSet::InitHealth(float NewVal); ==> SetBaseValue / SetCurrentValue
+ *	
+ * 用法：ATTRIBUTE_ACCESSORS(UMyHealthSet, Health)
+ */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+		GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/* 属性集可以有多个，本项目只使用一个*/
 UCLASS()
 class PROJECTGASRPG_API UMageAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	UMageAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/* 生命值 */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Mage_Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UMageAttributeSet, Health)
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Mage_Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UMageAttributeSet, MaxHealth)
+
+	/* 魔法值 */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Mage_Attributes")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UMageAttributeSet, Mana)
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Mage_Attributes")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UMageAttributeSet, MaxMana)
+
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	virtual void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	virtual void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
