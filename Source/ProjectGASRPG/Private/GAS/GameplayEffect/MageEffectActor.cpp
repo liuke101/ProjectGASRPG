d1f7e2dcc013710401219d97cdd7695f1009406c
liuke101 <226256089@qq.com>
2023-10-09 14:15:11 +0100

    GAS043 1. Effect Stacking 2.应用/移除Infinite Effect 3.重写Overlap逻辑

--- Source/ProjectGASRPG/Private/GAS/GameplayEffect/MageEffectActor.cpp
+++ Source/ProjectGASRPG/Private/GAS/GameplayEffect/MageEffectActor.cpp
@@ -20,16 +20,91 @@ void AMageEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 {
 	/* 获取目标Actor的 AbilitySystemComponent */
 	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
-	if (TargetASC == nullptr) return;
-	checkf(GameplayEffectClass, TEXT("GameplayEffectClass为空"));
-	
+	if (!IsValid(TargetASC)) return;
+	checkf(GameplayEffectClass, TEXT("GameplayEffectClass is nullptr"));
+
 	/* 创建GameplayEffectContextHandle */
 	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
 	EffectContextHandle.AddSourceObject(this);
 
 	/* EffectSpecHandle允许蓝图蓝图生成一个 GameplayEffectSpec，然后通过该句柄的共享指针 Data 引用它，以便多次应用/应用多个目标 */
-	FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.0f, EffectContextHandle);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(
+		GameplayEffectClass, 1.0f, EffectContextHandle);
 
 	/* 应用GameplayEffectSpec */
-	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); //注意第一个参数传的是引用类型，Get获取原始指针后还需要*解引用
+	const FActiveGameplayEffectHandle ActiveEffectHandle = TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); //注意第一个参数传的是引用类型，Get获取原始指针后还需要*解引用
+
+	/* 处理Infinite Effect */
+	const bool bIsInfinite =  EffectSpecHandle.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite;
+	if(bIsInfinite && InfiniteEffectRemovalPolicy == EffectRemovalPolicy::ERP_RemoveOnEndOverlap)
+	{
+		//用Map保存ActiveEffectHandle和对应的TargetActor的ASC，防止应用多个Infinite Effect时覆盖掉之前的Handle
+		ActiveEffectHandles.Add(ActiveEffectHandle, TargetASC); 
+	}
+}
+
+void AMageEffectActor::OnEffectBeginOverlap(AActor* TargetActor)
+{
+	if(IsValid(InstantGameplayEffectClass) && InstantEffectApplicationPolicy == EffectApplicationPolicy::EAP_ApplyOnBeginOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+	if(IsValid(DurationGameplayEffectClass) && DurationEffectApplicationPolicy == EffectApplicationPolicy::EAP_ApplyOnBeginOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+	if(IsValid(InfiniteGameplayEffectClass) && InfiniteEffectApplicationPolicy == EffectApplicationPolicy::EAP_ApplyOnBeginOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+}
+
+void AMageEffectActor::OnEffectEndOverlap(AActor* TargetActor)
+{
+	if (IsValid(InstantGameplayEffectClass) && InstantEffectApplicationPolicy == EffectApplicationPolicy::EAP_ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+	
+	if (IsValid(DurationGameplayEffectClass) && DurationEffectApplicationPolicy == EffectApplicationPolicy::EAP_ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+	
+	if(IsValid(InfiniteGameplayEffectClass))
+	{
+		if(InfiniteEffectApplicationPolicy == EffectApplicationPolicy::EAP_ApplyOnEndOverlap)
+		{
+			ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+		}
+		
+		if(InfiniteEffectRemovalPolicy == EffectRemovalPolicy::ERP_RemoveOnEndOverlap)
+		{
+			/* 从Map中移除对应的Handle和对应的Effect */
+			UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+			if (!IsValid(TargetASC)) return;
+		
+			//循环遍历map容器时，不建议直接删除元素，因为这可能会导致迭代器失效，引发未定义的行为(其他容器也要考虑这个情况)
+			//如果需要删除元素，可以先将需要删除的元素的Key值用TArray标记起来，然后在循环结束后再进行删除操作。
+			TArray<FActiveGameplayEffectHandle> KeysToDelete;
+			for (auto& Elem : ActiveEffectHandles)
+			{
+				if (Elem.Value == TargetASC)
+				{
+					TargetASC->RemoveActiveGameplayEffect(Elem.Key);
+					KeysToDelete.Add(Elem.Key);
+					break;
+				}
+			}
+
+			for(auto& Key : KeysToDelete)
+			{
+				ActiveEffectHandles.Remove(Key);
+			}
+		}
+	}
+	
+
+		
+	
 }
--- Source/ProjectGASRPG/Public/GAS/GameplayEffect/MageEffectActor.h
+++ Source/ProjectGASRPG/Public/GAS/GameplayEffect/MageEffectActor.h
@@ -1,11 +1,28 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
 #include "GameFramework/Actor.h"
 #include "MageEffectActor.generated.h"
 
+class UAbilitySystemComponent;
 class UGameplayEffect;
 class USphereComponent;
+
+UENUM(BlueprintType)
+enum class EffectApplicationPolicy : uint8
+{
+	EAP_ApplyOnBeginOverlap UMETA(DisplayName = "ApplyOnBeginOverlap"),
+	EAP_ApplyOnEndOverlap UMETA(DisplayName = "ApplyOnEndOverlap"),
+	EAP_DoNotApply UMETA(DisplayName = "DoNotApply")
+};
+
+UENUM(BlueprintType)
+enum class EffectRemovalPolicy : uint8
+{
+	ERP_RemoveOnEndOverlap UMETA(DisplayName = "RemoveOnEndOverlap"),
+	ERP_DoNotRemove UMETA(DisplayName = "DoNotRemove")
+};
 
 UCLASS()
 class PROJECTGASRPG_API AMageEffectActor : public AActor
@@ -20,10 +37,33 @@ protected:
 
 	UFUNCTION(BlueprintCallable, Category = "Mage_Effects")
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mage_Effects")
-	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mage_Effects")
+	UFUNCTION(BlueprintCallable, Category = "Mage_Effects")
+	void OnEffectBeginOverlap(AActor* TargetActor);
+	UFUNCTION(BlueprintCallable, Category = "Mage_Effects")
+	void OnEffectEndOverlap(AActor* TargetActor);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mage_Effects|Instant")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mage_Effects|Instant")
+	EffectApplicationPolicy InstantEffectApplicationPolicy = EffectApplicationPolicy::EAP_DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mage_Effects|Duration")
 	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mage_Effects|Duration")
+	EffectApplicationPolicy DurationEffectApplicationPolicy = EffectApplicationPolicy::EAP_DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mage_Effects|Infinite")
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mage_Effects|Infinite")
+	EffectApplicationPolicy InfiniteEffectApplicationPolicy = EffectApplicationPolicy::EAP_DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mage_Effects|Infinite")
+	EffectRemovalPolicy InfiniteEffectRemovalPolicy = EffectRemovalPolicy::ERP_RemoveOnEndOverlap;
+
+	TMap<FActiveGameplayEffectHandle,UAbilitySystemComponent*> ActiveEffectHandles;
+	
 };
