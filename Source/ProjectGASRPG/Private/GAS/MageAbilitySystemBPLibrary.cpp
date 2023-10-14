d78b6acdcb0e73c0bcaa9e7e06c37d30c84c4ca0
liuke101 <226256089@qq.com>
2023-10-14 10:12:23 +0100

    GAS089 MageAbilitySystemBPLibrary蓝图库

---
+++ Source/ProjectGASRPG/Private/GAS/MageAbilitySystemBPLibrary.cpp
@@ -0,0 +1,23 @@
+﻿// 
+
+
+#include "GAS/MageAbilitySystemBPLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "Player/MagePlayerState.h"
+#include "UI/HUD/MageHUD.h"
+#include "UI/WidgetController/MageWidgetController.h"
+
+UOverlayWidgetController* UMageAbilitySystemBPLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if(APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject,0))
+	{
+		if(AMageHUD* MageHUD = Cast<AMageHUD>(PC->GetHUD()))
+		{
+			AMagePlayerState* PS = Cast<AMagePlayerState>(PC->PlayerState);
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AttributeSet = PS->GetAttributeSet();
+			return MageHUD->GetOverlayWidgetController(FWidgetControllerParams(PC, PS, ASC, AttributeSet));	
+		}
+	}
+	return nullptr;
+}
---
+++ Source/ProjectGASRPG/Public/GAS/MageAbilitySystemBPLibrary.h
@@ -0,0 +1,20 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MageAbilitySystemBPLibrary.generated.h"
+
+class UOverlayWidgetController;
+/**
+ * 
+ */
+UCLASS()
+class PROJECTGASRPG_API UMageAbilitySystemBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintPure, Category = "Mage_AbilitySystemBPLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
