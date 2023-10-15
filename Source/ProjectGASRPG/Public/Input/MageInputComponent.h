c5d0da6e63567b270d46356ea3c8e022684f977a
liuke101 <226256089@qq.com>
2023-10-15 16:52:10 +0100

    GAS098 修正

--- Source/ProjectGASRPG/Public/Input/MageInputComponent.h
+++ Source/ProjectGASRPG/Public/Input/MageInputComponent.h
@@ -16,13 +16,14 @@ class PROJECTGASRPG_API UMageInputComponent : public UEnhancedInputComponent
 	GENERATED_BODY()
 
 public:
+	/* 绑定InputAction，同时支持 【按下】、【释放】、【持续】 三种回调函数 */
 	template <typename UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
-	void BindAbilityInputActions(const UMageInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, HeldFuncType HoldFunc, ReleasedFuncType ReleasedFunc);
+	void BindAbilityInputActions(const UMageInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc,HeldFuncType HoldFunc);
 };
 
 template <typename UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
 void UMageInputComponent::BindAbilityInputActions(const UMageInputConfig* InputConfig, UserClass* Object,
-	PressedFuncType PressedFunc, HeldFuncType HoldFunc, ReleasedFuncType ReleasedFunc)
+	PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HoldFunc)
 {
 	check(InputConfig);
 
@@ -35,17 +36,22 @@ void UMageInputComponent::BindAbilityInputActions(const UMageInputConfig* InputC
 			{
 				BindAction(Action.InputAction, ETriggerEvent::Started, Object, PressedFunc, Action.InputTag); //最后一个参数是绑定的Tag
 			}
-			if(HoldFunc)
-			{
-				BindAction(Action.InputAction, ETriggerEvent::Triggered, Object, HoldFunc, Action.InputTag); 
-			}
+			
 			if(ReleasedFunc)
 			{
 				BindAction(Action.InputAction, ETriggerEvent::Completed, Object, ReleasedFunc, Action.InputTag); //最后一个参数是绑定的Tag
 			}
+			
+			if(HoldFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Triggered, Object, HoldFunc, Action.InputTag); 
+			}
+			
 		}
 	}
 }
+
+
 
 
 
