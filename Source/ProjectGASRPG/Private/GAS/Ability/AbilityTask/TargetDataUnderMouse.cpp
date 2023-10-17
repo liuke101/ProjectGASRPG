96895b1e4860beaeb231ad6223e4de2d9ab7055b
liuke101 <226256089@qq.com>
2023-10-17 11:07:28 +0100

    GAS112 自定义Ability Evnet

---
+++ Source/ProjectGASRPG/Private/GAS/Ability/AbilityTask/TargetDataUnderMouse.cpp
@@ -0,0 +1,17 @@
+﻿#include "GAS/Ability/AbilityTask/TargetDataUnderMouse.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	return MyObj;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	if(const APlayerController* PlayerController = Ability->GetCurrentActorInfo()->PlayerController.Get())
+	{
+		FHitResult CursorHit;
+		PlayerController->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+		ValidData.Broadcast(CursorHit.ImpactPoint);
+	}
+}
---
+++ Source/ProjectGASRPG/Public/GAS/Ability/AbilityTask/TargetDataUnderMouse.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "TargetDataUnderMouse.generated.h"
+
+/** 节点的多个输出引脚都是由委托实现的 */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data);
+
+UCLASS()
+class PROJECTGASRPG_API UTargetDataUnderMouse : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks",meta = (DisplayName = "TargetDataUnderMouse", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "TRUE"))
+	static UTargetDataUnderMouse* CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility);
+
+	UPROPERTY(BlueprintAssignable)
+	FMouseTargetDataSignature ValidData;
+
+private:
+	virtual void Activate() override;
+};
