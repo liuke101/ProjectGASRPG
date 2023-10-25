7fd4b0d45558e07abb9759a446f7ce7bfa80c519
liuke101 <226256089@qq.com>
2023-10-25 16:54:16 +0100

    GAS147 创建 FGameplayEffectContext, 添加自定义数据

---
+++ Source/ProjectGASRPG/Private/GAS/MageAbilityTypes.cpp
@@ -0,0 +1,11 @@
+﻿#include "GAS/MageAbilityTypes.h"
+
+UScriptStruct* FMageGameplayEffectContext::GetScriptStruct() const
+{
+	return FGameplayEffectContext::GetScriptStruct();
+}
+
+bool FMageGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	return FGameplayEffectContext::NetSerialize(Ar, Map, bOutSuccess);
+}
---
+++ Source/ProjectGASRPG/Public/GAS/MageAbilityTypes.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "UObject/Object.h"
+#include "MageAbilityTypes.generated.h"
+
+/* 继承 FGameplayEffectContext, 添加自定义数据 */
+USTRUCT()
+struct FMageGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+
+	FORCEINLINE bool GetIsCriticalHit() const { return bIsCriticalHit; }
+	FORCEINLINE void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+
+	
+	/** 返回用于序列化的实际结构体，子类必须重载该函数 */
+	virtual UScriptStruct* GetScriptStruct() const override;
+
+	/** 自定义序列化，子类必须重载该函数 */
+	virtual bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess) override;
+
+protected:
+
+	/** 是否暴击 */
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
+
