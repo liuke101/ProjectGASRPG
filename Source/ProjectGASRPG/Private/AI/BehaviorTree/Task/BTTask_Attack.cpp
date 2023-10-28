b3717c5deec17f9287dff01cf0db7774a4cf8679
liuke101 <226256089@qq.com>
2023-10-28 16:54:36 +0100

    GAS167 行为树任务

---
+++ Source/ProjectGASRPG/Private/AI/BehaviorTree/Task/BTTask_Attack.cpp
@@ -0,0 +1,6 @@
+﻿#include "AI/BehaviorTree/Task/BTTask_Attack.h"
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
---
+++ Source/ProjectGASRPG/Public/AI/BehaviorTree/Task/BTTask_Attack.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlueprintBase.h"
+#include "BTTask_Attack.generated.h"
+
+UCLASS()
+class PROJECTGASRPG_API UBTTask_Attack : public UBTTask_BlueprintBase
+{
+	GENERATED_BODY()
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+};
