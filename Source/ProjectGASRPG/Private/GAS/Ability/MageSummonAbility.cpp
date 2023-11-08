8de2dfa24a4a63908c66162c09b2e79b5bff1eca
liuke101 <226256089@qq.com>
2023-11-08 23:00:10 +0000

    GAS2-16 萨满召唤Ability，实现SpawnLocation算法

---
+++ Source/ProjectGASRPG/Private/GAS/Ability/MageSummonAbility.cpp
@@ -0,0 +1,26 @@
+﻿#include "GAS/Ability/MageSummonAbility.h"
+
+TArray<FVector> UMageSummonAbility::GetSpawnLocations() const
+{
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const float DeltaSpread = SpawnSpread / SummonCount;
+	
+	// 绕轴旋转(Z轴（UpVector）旋转方向遵循左手定则，顺时针为正)
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread/2.f, FVector::UpVector);
+	TArray<FVector> SpawnLocations;
+	
+	for(int32 i =0;i<SummonCount;i++)
+	{
+		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+		const FVector SpawnLocation = Location + Direction * FMath::RandRange(MinSpawnDistance, MaxSpawnDistance);
+		SpawnLocations.Add(SpawnLocation);
+
+		DrawDebugSphere(GetWorld(),Location+Direction*MinSpawnDistance,10.f,8,FColor::Red,false,5.f);
+		DrawDebugSphere(GetWorld(),Location+Direction*MaxSpawnDistance,10.f,8,FColor::Green,false,5.f);
+
+		DrawDebugSphere(GetWorld(),SpawnLocation,10.f,8,FColor::Yellow,false,5.f);
+	}
+	
+	return SpawnLocations;
+}
---
+++ Source/ProjectGASRPG/Public/GAS/Ability/MageSummonAbility.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MageGameplayAbility.h"
+#include "MageSummonAbility.generated.h"
+
+UCLASS()
+class PROJECTGASRPG_API UMageSummonAbility : public UMageGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocations() const;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mage_Summon")
+	TArray<TSubclassOf<APawn>> SummonClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mage_Summon")
+	int32 SummonCount = 5;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mage_Summon")
+	float MinSpawnDistance = 100.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mage_Summon")
+	float MaxSpawnDistance = 200.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mage_Summon")
+	float SpawnSpread = 90.0f;
+};
