40bd16ea06bd25b3952040d176d72d1f2fa795d1
liuke101 <226256089@qq.com>
2023-11-08 23:12:39 +0000

    GAS2-17 异步召唤（delay）+ 射线检测保证生成在地面

--- Source/ProjectGASRPG/Private/GAS/Ability/MageSummonAbility.cpp
+++ Source/ProjectGASRPG/Private/GAS/Ability/MageSummonAbility.cpp
@@ -4,7 +4,7 @@ TArray<FVector> UMageSummonAbility::GetSpawnLocations() const
 {
 	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
 	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
-	const float DeltaSpread = SpawnSpread / SummonCount;
+	const float DeltaSpread = SpawnSpread / SummonCount; //每个召唤物之间的角度差
 	
 	// 绕轴旋转(Z轴（UpVector）旋转方向遵循左手定则，顺时针为正)
 	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread/2.f, FVector::UpVector);
@@ -13,13 +13,18 @@ TArray<FVector> UMageSummonAbility::GetSpawnLocations() const
 	for(int32 i =0;i<SummonCount;i++)
 	{
 		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
-		const FVector SpawnLocation = Location + Direction * FMath::RandRange(MinSpawnDistance, MaxSpawnDistance);
+		FVector SpawnLocation = Location + Direction * FMath::RandRange(MinSpawnDistance, MaxSpawnDistance);
+		
+		/** 通过射线检测, 保证召唤物生成在地面上 */
+		FHitResult HitResult;
+		GetWorld()->LineTraceSingleByChannel(HitResult,SpawnLocation + FVector(0,0,400),SpawnLocation - FVector(0,0,400),ECC_Visibility);
+		if(HitResult.bBlockingHit)
+		{
+			SpawnLocation = HitResult.ImpactPoint;
+		}
+		
 		SpawnLocations.Add(SpawnLocation);
-
-		DrawDebugSphere(GetWorld(),Location+Direction*MinSpawnDistance,10.f,8,FColor::Red,false,5.f);
-		DrawDebugSphere(GetWorld(),Location+Direction*MaxSpawnDistance,10.f,8,FColor::Green,false,5.f);
-
-		DrawDebugSphere(GetWorld(),SpawnLocation,10.f,8,FColor::Yellow,false,5.f);
+		
 	}
 	
 	return SpawnLocations;
